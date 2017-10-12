4b57e3754d9653a8106620ea7c93237fdf546285
benzkji <bnzk@bnzk.ch>
2017-10-12 16:20:48 +0200

    with autopublisher, no signals are needed!

--- djangocms_misc/global_untranslated_placeholder/signals.py
+++ djangocms_misc/global_untranslated_placeholder/signals.py
@@ -10,43 +10,44 @@ from cms.signals import post_placeholder_operation, post_obj_operation, post_pub
 from djangocms_misc.global_untranslated_placeholder.utils import get_untranslated_default_language
 
 
-# published. if not default language, publish default as well
-@receiver(
-    post_publish,
-    dispatch_uid="cms_global_untranslated_placeholder_post_publish",
-)
-def post_publish_handler(**kwargs):
-    page_instance = kwargs.get('instance', None)
-    if not page_instance:
-        return
-    published_language = kwargs.get('language', settings.LANGUAGE_CODE)
-    default_language = get_untranslated_default_language()
-    if not published_language == default_language:
-        page_instance.publish(default_language)
+if not 'djangocms_misc.autopublisher' in settings.INSTALLED_APPS:
+
+    # published. if not default language, publish default as well
+    @receiver(
+        post_publish,
+        dispatch_uid="cms_global_untranslated_placeholder_post_publish",
+    )
+    def post_publish_handler(**kwargs):
+        page_instance = kwargs.get('instance', None)
+        if not page_instance:
+            return
+        published_language = kwargs.get('language', settings.LANGUAGE_CODE)
+        default_language = get_untranslated_default_language()
+        if not published_language == default_language:
+            page_instance.publish(default_language)
 
 
-
-# something has changed, if we are not on the default lang, mark current lang/title as dirty as well
-@receiver(
-    post_placeholder_operation,
-    dispatch_uid="cms_global_untranslated_placeholder_post_placeholder_operation",
-)
-def post_placeholder_operation_handler(sender, operation, request, language, token, origin, **kwargs):
-    plugin = None
-    if operation == 'change_plugin':
-        plugin = kwargs.get('new_plugin', None)
-    if not plugin:
-        plugin = kwargs.get('plugin', None)
-    if not plugin:
-        plugin = kwargs.get('plugins', [None, ])[0]
-    print operation
-    print plugin
-    print kwargs
-    if plugin:
-        placeholder = plugin.placeholder
-        if not plugin.language == language:
-            # mark current language Title as dirty as well!
-            if placeholder.page:
-                title = placeholder.page.get_title_obj(language)
-                title.publisher_state = PUBLISHER_STATE_DIRTY
-                title.save()
+    # something has changed, if we are not on the default lang, mark current lang/title as dirty as well
+    @receiver(
+        post_placeholder_operation,
+        dispatch_uid="cms_global_untranslated_placeholder_post_placeholder_operation",
+    )
+    def post_placeholder_operation_handler(sender, operation, request, language, token, origin, **kwargs):
+        plugin = None
+        if operation == 'change_plugin':
+            plugin = kwargs.get('new_plugin', None)
+        if not plugin:
+            plugin = kwargs.get('plugin', None)
+        if not plugin:
+            plugin = kwargs.get('plugins', [None, ])[0]
+        print operation
+        print plugin
+        print kwargs
+        if plugin:
+            placeholder = plugin.placeholder
+            if not plugin.language == language:
+                # mark current language Title as dirty as well!
+                if placeholder.page:
+                    title = placeholder.page.get_title_obj(language)
+                    title.publisher_state = PUBLISHER_STATE_DIRTY
+                    title.save()
