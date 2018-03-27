0df7039781b9666f41ff5750a5a2cee3c854fdcd
benzkji <bnzk@bnzk.ch>
2018-03-27 15:43:03 +0200

    cms 3.5 quickfix

--- djangocms_misc/alternate_toolbar/cms_toolbars.py
+++ djangocms_misc/alternate_toolbar/cms_toolbars.py
@@ -98,7 +98,11 @@ class AlternateBasicToolbar(BasicToolbar):
         # buttons / items
         if self.toolbar.edit_mode or self.toolbar.build_mode:
             # True if the clipboard exists and there's plugins in it.
-            clipboard_is_bound = self.get_clipboard_plugins().exists()
+            if getattr(self, 'get_clipboard_plugins', None):
+                # cms up to 4.4.6
+                clipboard_is_bound = self.get_clipboard_plugins().exists()
+            else:
+                clipboard_is_bound = self.toolbar.clipboard_plugin
             self.clipboard_menu.add_link_item(
                 _('Clipboard...'),
                 url='#',
