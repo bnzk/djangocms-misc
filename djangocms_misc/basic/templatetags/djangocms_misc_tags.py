5dd1b6a3e5219a11760e37668cc025151a56954d
benzkji <bnzk@bnzk.ch>
2018-05-23 16:48:48 +0200

    go

--- djangocms_misc/basic/templatetags/djangocms_misc_tags.py
+++ djangocms_misc/basic/templatetags/djangocms_misc_tags.py
@@ -10,6 +10,9 @@ register = template.Library()
 
 @register.inclusion_tag('djangocms_misc/tags/page_link.html', takes_context=True)
 def djangocms_misc_page_link(context, lookup, css_class='', link_text='', link_text_attr=''):
+    """
+    link_text_attr is not working (yet)
+    """
     if not link_text_attr:
         link_text_attr = 'title'
     context.update({
