d47c803db595e7c1c6f32698a00f352452f6ba61
benzkji <bnzk@bnzk.ch>
2018-07-26 16:48:45 +0200

    prepare redirect subpage

---
+++ djangocms_misc/basic/middleware/redirect_subpage.py
@@ -0,0 +1,27 @@
+from django.shortcuts import redirect
+
+
+class RedirectFirstSubpageMiddleware(object):
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        response = self.get_response(request)
+        # Code to be executed for each request/response after
+        # the view is called.
+        return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        if getattr(request, 'current_page', None):
+            the_page = request.current_page
+            the_redirect = the_page.get_redirect()
+            if view_func.__name__ == 'details' and "slug" in view_kwargs and the_redirect == "/firstchild":
+                subpages = request.current_page.children.all()[:1]
+                if len(subpages)>0:
+                    # can redirect! must make uri!
+                    return redirect(subpages[0].get_absolute_url(), permanent=True)
+        return None
