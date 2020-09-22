7e62e4dd69021348a6bb2e3bcb6882b914cd81a2
Ben Stähli <bnzk@bnzk.ch>
2020-09-22 15:41:06 +0200

    get_model replaced

--- djangocms_misc/basic/app_template/views_utils.py
+++ djangocms_misc/basic/app_template/views_utils.py
@@ -33,5 +33,5 @@ class PublishedViewMixin():
     """
     def get_queryset(self):
         if self.request.toolbar.edit_mode:
-            return self.get_model().objects.all()
-        return self.get_model().objects.published()
+            return self.model.objects.all()
+        return self.model.objects.published()
