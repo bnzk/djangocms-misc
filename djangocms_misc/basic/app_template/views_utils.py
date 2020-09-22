359b5f0afe974acaeda794770a8d15347d4f8dce
Ben Stähli <bnzk@bnzk.ch>
2020-09-22 15:33:24 +0200

    add view mixins

---
+++ djangocms_misc/basic/app_template/views_utils.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponsePermanentRedirect
+
+"""
+some view mixins, to use with django-cms
+"""
+
+
+class LanguageChooserEnhancerMixin(object):
+    """
+    this calls correct get_absolute_urls in cms's language_chooser tags
+    """
+    def get(self, request, **kwargs):
+        self.object = self.get_object()
+        if hasattr(self.request, 'toolbar'):
+            self.request.toolbar.set_object(self.object)
+        return super(LanguageChooserEnhancerMixin, self).get(request, **kwargs)
+
+
+class AutoSlugMixin(object):
+    """
+    redirect if the slug is no more correct
+    """
+    def get(self, request, **kwargs):
+        self.object = self.get_object()
+        if self.request.path != self.object.get_absolute_url():
+            return HttpResponsePermanentRedirect(self.object.get_absolute_url())
+        return super(AutoSlugMixin, self).get(request, **kwargs)
+
+
+class PublishedViewMixin():
+    """
+    in edit mode, get all, otherwise only published
+    """
+    def get_queryset(self):
+        if self.request.toolbar.edit_mode:
+            return self.get_model().objects.all()
+        return self.get_model().objects.published()
