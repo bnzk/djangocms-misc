f87ce65114864c04e01cdcb109cfd2fbf779bf6f
Ben Stähli <bnzk@bnzk.ch>
2020-09-28 15:21:49 +0200

    language tabs for tabular inlinex

--- djangocms_misc/tests/test_app/admin.py
+++ djangocms_misc/tests/test_app/admin.py
@@ -1,19 +1,21 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationTabularInline, TranslationStackedInline, TranslationAdmin
 
 from .models import TestModel, TestInlineModel
+from ...basic.admin import LanguageTabsMixin
 
 
-class TestInlineModelInline(admin.StackedInline):
+class TestInlineModelInline(TranslationStackedInline, admin.StackedInline):
     model = TestInlineModel
     extra = 2
 
 
-class TestInlineModelInline2(admin.TabularInline):
+class TestInlineModelInline2(TranslationTabularInline, admin.TabularInline):
     model = TestInlineModel
     extra = 2
 
 
-class TestModelAdmin(admin.ModelAdmin):
+class TestModelAdmin(LanguageTabsMixin, TranslationAdmin, admin.ModelAdmin):
     inlines = [TestInlineModelInline, TestInlineModelInline2, ]
     fieldsets = [
         ['', {'fields': ['field0', ]}],
--- djangocms_misc/tests/test_app/translation.py
+++ djangocms_misc/tests/test_app/translation.py
@@ -1,5 +1,5 @@
 from modeltranslation.translator import translator, TranslationOptions
-from .models import TestPluginModel
+from .models import TestPluginModel, TestModel, TestInlineModel
 
 
 class TestPluginModelTranslationOptions(TranslationOptions):
@@ -7,3 +7,17 @@ class TestPluginModelTranslationOptions(TranslationOptions):
 
 
 translator.register(TestPluginModel, TestPluginModelTranslationOptions)
+
+
+class TestModelTranslationOptions(TranslationOptions):
+    fields = ('field1', )
+
+
+translator.register(TestModel, TestModelTranslationOptions)
+
+
+class TestInlineModelTranslationOptions(TranslationOptions):
+    fields = ('field1', )
+
+
+translator.register(TestInlineModel, TestInlineModelTranslationOptions)
