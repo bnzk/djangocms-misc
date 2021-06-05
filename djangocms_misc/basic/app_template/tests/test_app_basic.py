b86c6ef904df5f0c3187b0d73247b48f3ef30c04
Ben Stähli <bnzk@bnzk.ch>
2021-06-05 14:35:26 +0200

    no -e

--- djangocms_misc/basic/app_template/tests/test_app_basic.py
+++ djangocms_misc/basic/app_template/tests/test_app_basic.py
@@ -2,7 +2,9 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+from ..models import AppTemplate
+
 
 # add some basic things (admin list view, detail view, etc)
-class AppTests(TestCase):
+class AppTemplateTests(TestCase):
     pass
