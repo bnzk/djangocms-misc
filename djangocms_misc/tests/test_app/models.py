fc8dda44fd88a7e804b920a6f79e472669cc0312
Ben Stähli <bnzk@bnzk.ch>
2021-06-05 14:22:32 +0200

    up to date again, added githuba actions

--- djangocms_misc/tests/test_app/models.py
+++ djangocms_misc/tests/test_app/models.py
@@ -1,11 +1,7 @@
-from __future__ import unicode_literals
-
 from cms.models import CMSPlugin
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 
-@python_2_unicode_compatible
 class TestPluginModel(CMSPlugin):
     field1 = models.CharField(max_length=64, default='', blank=False)
     field_date = models.DateField(default=None, null=True, )
@@ -16,7 +12,6 @@ class TestPluginModel(CMSPlugin):
         return self.field1
 
 
-@python_2_unicode_compatible
 class TestModel(models.Model):
     field0 = models.CharField(max_length=64, default='', blank=True)
     field1 = models.CharField(max_length=64, default='', blank=False)
@@ -26,7 +21,6 @@ class TestModel(models.Model):
         return self.field1
 
 
-@python_2_unicode_compatible
 class TestInlineModel(models.Model):
     testmodel = models.ForeignKey(TestModel, on_delete=models.CASCADE)
     field1 = models.CharField(max_length=64, default='', blank=False)
