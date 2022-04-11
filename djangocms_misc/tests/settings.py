9e51ce8149b8c0a46416597989af08ef96c7549b
Ben Stähli <bnzk@bnzk.ch>
2022-04-11 23:16:46 +0200

    dev again, autofield for tests/testapp

--- djangocms_misc/tests/settings.py
+++ djangocms_misc/tests/settings.py
@@ -13,6 +13,8 @@ SITE_ID = 1
 # from selenium.webdriver.phantomjs import webdriver
 # SELENIUM_WEBDRIVER = webdriver
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 APP_ROOT = os.path.abspath(
     os.path.join(os.path.dirname(__file__), ".."))
 
