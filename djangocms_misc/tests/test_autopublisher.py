8b0ecf874785bc73e614eabd6c1d0fc4b9002d72
Ben Stähli <bnzk@bnzk.ch>
2025-05-27 14:44:36 +0200

    fix tests

--- djangocms_misc/tests/test_autopublisher.py
+++ djangocms_misc/tests/test_autopublisher.py
@@ -1,6 +1,9 @@
-# -*- coding: utf-8 -*-
+from cms.models import Page
 from cms.test_utils.testcases import CMSTestCase
+from cms.utils.urlutils import admin_reverse
+from django.contrib.auth.models import User
 from django.test import TestCase, Client, modify_settings
+from django.utils.http import urlencode
 
 from djangocms_misc.tests.test_app.cms_plugins import TestPlugin
 
@@ -8,10 +11,13 @@ from djangocms_misc.tests.test_app.cms_plugins import TestPlugin
 @modify_settings(INSTALLED_APPS={
     'append': 'djangocms_misc.autopublisher',
 })
-class AutoPublisherTestCase(CMSTestCase, TestCase):
+class AutoPublisherTestCase(TestCase):
+    password = "123"
     def setUp(self):
+        super().setUp()
         self.client = Client()
-        self._admin_user = self.get_superuser()
+        # self._admin_user = self.get_superuser()
+        self._admin_user = User.objects.create_superuser(username='admin', email=None, password=self.password)
 
     def tearDown(self):
         pass
@@ -76,9 +82,12 @@ class AutoPublisherTestCase(CMSTestCase, TestCase):
             'placeholder_id': placeholder.id,
             'plugin_order': [plugin2.id, plugin1.id, ],
         }
-        with self.login_user_context(self._admin_user):
-            response = self.client.post(uri, data)
-            self.assertEqual(response.status_code, 200)
+        # with self.login_user_context(self._admin_user):
+        #     response = self.client.post(uri, data)
+        #     self.assertEqual(response.status_code, 200)
+        self.client.force_login(self._admin_user)
+        response = self.client.post(uri, data)
+        self.assertEqual(response.status_code, 200)
         response = self.client.get(page.get_absolute_url())
         # helps debugging, when plugin output might have change slightly!
         # print(response.content)
@@ -88,6 +97,24 @@ class AutoPublisherTestCase(CMSTestCase, TestCase):
     testplugin content: teststring
 </div>
 ''')
+
+    def get_admin_url(self, model, action, *args):
+        opts = model._meta
+        url_name = f"{opts.app_label}_{opts.model_name}_{action}"
+        return admin_reverse(url_name, args=args)
+
+    def get_move_plugin_uri(self, plugin, container=None, language=None):
+        container = container or Page
+        language = language or 'en'
+
+        if plugin.page:
+            path = plugin.page.get_absolute_url(language) or f'/{language}/'
+        else:
+            path = f'/{language}/'
+
+        endpoint = self.get_admin_url(container, 'move_plugin')
+        endpoint += '?' + urlencode({'cms_path': path})
+        return endpoint
 
     def test_move_page(self):
         pass
