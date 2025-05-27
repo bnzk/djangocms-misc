403b670321107d284301d3e961c5641ec5575525
Ben Stähli <bnzk@bnzk.ch>
2025-05-27 15:23:15 +0200

    pre-commit fixes

--- djangocms_misc/tests/test_autopublisher.py
+++ djangocms_misc/tests/test_autopublisher.py
@@ -105,7 +105,6 @@ class AutoPublisherTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         response = self.client.get(page.get_absolute_url())
         # helps debugging, when plugin output might have change slightly!
-        print(response.content)
         # print(response.content)
         self.assertContains(
             response,
